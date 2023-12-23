35218d4ed0147122cab8bc2b8403f9662c74bcc1
Josue <josueadrianib@gmail.com>
2023-12-22 22:49:04 -0500

    uiautomator

--- src/test/java/org/example/screens/BaseScreen.java
+++ src/test/java/org/example/screens/BaseScreen.java
@@ -1,13 +1,37 @@
 package org.example.screens;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.Test;
 
 public class BaseScreen {
+
+
+
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.wdiodemoapp:id/action_bar_root\").childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().className(\"android.view.ViewGroup\")).instance(0)).childSelector(new UiSelector().className(\"android.widget.Button\").description(\"Home\"))")
+    protected WebElement homeButton;
+
+
+    //todas las pantallas tienen estoso botones
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.wdiodemoapp:id/action_bar_root\").childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().className(\"android.view.ViewGroup\")).instance(0)).childSelector(new UiSelector().className(\"android.widget.Button\").description(\"Webview\"))")
+    protected WebElement webButton;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.wdiodemoapp:id/action_bar_root\").childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().className(\"android.view.ViewGroup\")).instance(0)).childSelector(new UiSelector().className(\"android.widget.Button\").description(\"Login\"))")
+    protected WebElement loginButton;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.wdiodemoapp:id/action_bar_root\").childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().className(\"android.view.ViewGroup\")).instance(0)).childSelector(new UiSelector().className(\"android.widget.Button\").description(\"Forms\"))")
+    protected WebElement formsButton;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.wdiodemoapp:id/action_bar_root\").childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().className(\"android.view.ViewGroup\")).instance(0)).childSelector(new UiSelector().className(\"android.widget.Button\").description(\"Swipe\"))")
+    protected WebElement swipeButton;
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.wdiodemoapp:id/action_bar_root\").childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().className(\"android.view.ViewGroup\")).instance(0)).childSelector(new UiSelector().className(\"android.widget.Button\").description(\"Drag\"))")
+    protected WebElement dragButton;
+
 
     protected AndroidDriver driver;
 
@@ -17,5 +41,36 @@ public class BaseScreen {
     }
 
 
+//    public HomeScreen navigateToHomeScreen() {
+//        homeButton.click();
+//        return new HomeScreen(driver);
+//    }
+//
+//    public WebScreen navigateToWebScreen() {
+//        webButton.click();
+//        return new WebScreen(driver);
+//    }
+//
+//    public LoginScreen navigateToLoginScreen() {
+//        loginButton.click();
+//        return new LoginScreen(driver);
+//    }
+//
+//    public FormScreen navigateToFormScreen() {
+//        formsButton.click();
+//        return new FormScreen(driver);
+//    }
+//
+//    public SwipeScreen navigateToSwipeScreen() {
+//        swipeButton.click();
+//        return new SwipeScreen(driver);
+//    }
+//
+//    public DragScreen navigateToDragScreen() {
+//        dragButton.click();
+//        return new DragScreen(driver);
+//    }
+
 }
+
 
