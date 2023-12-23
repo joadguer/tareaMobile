219bbb013f2599fffec3d8f8ad7abcadc8f8d007
Josue <josueadrianib@gmail.com>
2023-12-22 23:05:58 -0500

    uiselectors correctos

--- src/test/java/org/example/screens/BaseScreen.java
+++ src/test/java/org/example/screens/BaseScreen.java
@@ -17,19 +17,20 @@ public class BaseScreen {
 
     //todas las pantallas tienen estoso botones
 
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.wdiodemoapp:id/action_bar_root\").childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().className(\"android.view.ViewGroup\")).instance(0)).childSelector(new UiSelector().className(\"android.widget.Button\").description(\"Webview\"))")
+    @AndroidFindBy(uiAutomator = "new UiSelector().resourceIdMatches(\".*id/action_bar_root\").childSelector(new UiSelector().resourceIdMatches(\".*id/content\")).childSelector(new UiSelector().descriptionMatches(\".*Webview\"))")
     protected WebElement webButton;
 
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.wdiodemoapp:id/action_bar_root\").childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().className(\"android.view.ViewGroup\")).instance(0)).childSelector(new UiSelector().className(\"android.widget.Button\").description(\"Login\"))")
+
+    @AndroidFindBy(uiAutomator = "new UiSelector().resourceIdMatches(\".*id/action_bar_root\").childSelector(new UiSelector().resourceIdMatches(\".*id/content\")).childSelector(new UiSelector().descriptionMatches(\".*Login\"))")
     protected WebElement loginButton;
 
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.wdiodemoapp:id/action_bar_root\").childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().className(\"android.view.ViewGroup\")).instance(0)).childSelector(new UiSelector().className(\"android.widget.Button\").description(\"Forms\"))")
+    @AndroidFindBy(uiAutomator = "new UiSelector().resourceIdMatches(\".*id/action_bar_root\").childSelector(new UiSelector().resourceIdMatches(\".*id/content\")).childSelector(new UiSelector().descriptionMatches(\".*Forms\"))")
     protected WebElement formsButton;
 
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.wdiodemoapp:id/action_bar_root\").childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().className(\"android.view.ViewGroup\")).instance(0)).childSelector(new UiSelector().className(\"android.widget.Button\").description(\"Swipe\"))")
+    @AndroidFindBy(uiAutomator = "new UiSelector().resourceIdMatches(\".*id/action_bar_root\").childSelector(new UiSelector().resourceIdMatches(\".*id/content\")).childSelector(new UiSelector().descriptionMatches(\".*Swipe\"))")
     protected WebElement swipeButton;
 
-    @AndroidFindBy(uiAutomator = "new UiSelector().resourceId(\"com.wdiodemoapp:id/action_bar_root\").childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().resourceId(\"android:id/content\")).childSelector(new UiSelector().className(\"android.view.ViewGroup\")).instance(0)).childSelector(new UiSelector().className(\"android.widget.Button\").description(\"Drag\"))")
+    @AndroidFindBy(uiAutomator = "new UiSelector().resourceIdMatches(\".*id/action_bar_root\").childSelector(new UiSelector().resourceIdMatches(\".*id/content\")).childSelector(new UiSelector().descriptionMatches(\".*Drag\"))")
     protected WebElement dragButton;
 
 
@@ -41,35 +42,35 @@ public class BaseScreen {
     }
 
 
-//    public HomeScreen navigateToHomeScreen() {
-//        homeButton.click();
-//        return new HomeScreen(driver);
-//    }
-//
-//    public WebScreen navigateToWebScreen() {
-//        webButton.click();
-//        return new WebScreen(driver);
-//    }
-//
-//    public LoginScreen navigateToLoginScreen() {
-//        loginButton.click();
-//        return new LoginScreen(driver);
-//    }
-//
-//    public FormScreen navigateToFormScreen() {
-//        formsButton.click();
-//        return new FormScreen(driver);
-//    }
-//
-//    public SwipeScreen navigateToSwipeScreen() {
-//        swipeButton.click();
-//        return new SwipeScreen(driver);
-//    }
-//
-//    public DragScreen navigateToDragScreen() {
-//        dragButton.click();
-//        return new DragScreen(driver);
-//    }
+    public HomeScreen navigateToHomeScreen() {
+        homeButton.click();
+        return new HomeScreen(driver);
+    }
+
+    public WebScreen navigateToWebScreen() {
+        webButton.click();
+        return new WebScreen(driver);
+    }
+
+    public LoginScreen navigateToLoginScreen() {
+        loginButton.click();
+        return new LoginScreen(driver);
+    }
+
+    public FormScreen navigateToFormScreen() {
+        formsButton.click();
+        return new FormScreen(driver);
+    }
+
+    public SwipeScreen navigateToSwipeScreen() {
+        swipeButton.click();
+        return new SwipeScreen(driver);
+    }
+
+    public DragScreen navigateToDragScreen() {
+        dragButton.click();
+        return new DragScreen(driver);
+    }
 
 }
 
--- src/test/java/org/example/tests/NavigationTest.java
+++ src/test/java/org/example/tests/NavigationTest.java
@@ -1,16 +1,34 @@
 package org.example.tests;
 
-import org.example.screens.HomeScreen;
+import org.example.screens.*;
 import org.testng.annotations.Test;
 
 public class NavigationTest extends BaseTest{
 
     @Test
     public void changeMenuOption(){
+        // Start from the HomeScreen
         HomeScreen homeScreen = returnHomeScreen();
+
+        // Navigate to WebScreen
+        WebScreen webScreen = homeScreen.navigateToWebScreen();
+        // Perform actions/assertions for WebScreen
+        // Navigate to LoginScreen
+        LoginScreen loginScreen = webScreen.navigateToLoginScreen();
+        // Perform actions/assertions for LoginScreen
+
+        // Navigate to FormScreen
+        FormScreen formScreen = loginScreen.navigateToFormScreen();
+        // Perform actions/assertions for FormScreen
+
+        // Navigate to SwipeScreen
+        SwipeScreen swipeScreen = formScreen.navigateToSwipeScreen();
+        // Perform actions/assertions for SwipeScreen
+
+        // Navigate to DragScreen
+        DragScreen dragScreen = swipeScreen.navigateToDragScreen();
+        // Perform actions/assertions for DragScreen
+
     }
-
-
-
 
 }
